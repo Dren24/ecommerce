c8fbbc2ab9ea4656d0f72093dec68696f321b23e
Dren24 <baricuatroaldren98@gmail.com>
2025-11-25 13:44:18 +0800

    chp10-ecommerce

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? 'hiii' }}</title>  
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles                                                                                         
+    </head>
+    <body class="bg-gray-100 min-h-screen dark:bg-gray-900">
+        <main>
+        {{ $slot }}
+        @livewireScripts
+        </main>
+    </body>
+</html>
---
+++ resources/views/livewire/homepage.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>hiii</h1>{{-- Stop trying to control. --}}
+</div>
