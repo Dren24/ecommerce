be8ebedc62c2f3ad44063cba3205a397ddd6f286
Dren24 <baricuatroaldren98@gmail.com>
2025-12-02 21:34:55 +0800

    Revert "chp14to17-ecommerce"
    This reverts commit b9c64f11aca30b4df78cae3d44fd3ab1bcee298d.

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -6,7 +6,6 @@ use Livewire\Component;
 
 class MyOrdersPage extends Component
 {
-    #[\Livewire\Attributes\Title('My Orders')]
     public function render()
     {
         return view('livewire.my-orders-page');
--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,94 +2,12 @@
 
 namespace App\Livewire;
 
-use App\Helpers\CartManagement;
-use App\Livewire\Partials\Navbar;
-use App\Models\Brand;
-use App\Models\Category;
-use App\Models\Product;
-use Jantinnerezo\LivewireAlert\Facades\LivewireAlert; // Facade
 use Livewire\Component;
-use Livewire\WithPagination;
-use Livewire\Attributes\Title;
-use Livewire\Attributes\Url;
 
 class ProductsPage extends Component
 {
-    use WithPagination;
-
-    #[Title('Products Page')]
-    #[Url()]
-    public $selectedCategories = [];
-
-    #[Url()]
-    public $selectedBrands = [];
-
-    #[Url()]
-    public $featured;
-
-    #[Url()]
-    public $onSale;
-
-    #[Url()]
-    public $priceRange = 0;
-
-    #[Url()]
-    public $sort = 'latest';
-
-    // Add products to cart
-    public function addToCart($product_id)
-    {
-        $total_count = CartManagement::addItemsToCart($product_id);
-
-        // Update cart count in Navbar
-        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
-
-        // LivewireAlert v4 (Facade)
-        LivewireAlert::title('Success')
-            ->text('Product added to cart successfully!')
-            ->success()
-            ->toast()
-            ->position('bottom-end')
-            ->timer(3000)
-            ->show();
-    }
-
     public function render()
     {
-        $products = Product::query()->where('is_active', true);
-
-        if (!empty($this->selectedCategories)) {
-            $products->whereIn('category_id', $this->selectedCategories);
-        }
-
-        if (!empty($this->selectedBrands)) {
-            $products->whereIn('brand_id', $this->selectedBrands);
-        }
-
-        if ($this->featured) {
-            $products->where('is_featured', true);
-        }
-
-        if ($this->onSale) {
-            $products->where('on_sale', true);
-        }
-
-        if ($this->priceRange) {
-            $products->whereBetween('price', [0, $this->priceRange]);
-        }
-
-        if ($this->sort == 'price') {
-            $products->orderBy('price');
-        }
-
-        if ($this->sort == 'latest') {
-            $products->latest();
-        }
-
-        return view('livewire.products-page', [
-            'products' => $products->paginate(4),
-            'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
-            'categories' => Category::query()->where('is_active', 1)->get(['id', 'name', 'slug']),
-        ]);
+        return view('livewire.products-page');
     }
 }
